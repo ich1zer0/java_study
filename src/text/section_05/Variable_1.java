b31b5f95854824e67da8fbfe3f14819a949ae49f
ich1zer0 <proton.ich1@protonmail.com>
2024-09-16 14:41:39 +0900

    Section5 コード追加

---
+++ src/text/section_05/Variable_1.java
@@ -0,0 +1,8 @@
+package text.section_05;
+
+public class Variable_1 {
+    public static void main(String[] args) {
+        int test = 123;
+        System.out.println(test);
+    }
+}
---
+++ src/text/section_05/Variable_2.java
@@ -0,0 +1,28 @@
+package text.section_05;
+
+public class Variable_2 {
+    public static void main(String[] args) {
+        byte testByte = 1;
+        short testShort = 12;
+        int testInt = 123;
+        long testLong = 1234;
+
+        float testFloat = 1.23F;
+        double testDouble = 1.23456;
+
+        boolean testBoolean = true;
+
+        String testString = "Samurai";
+        char testChar = 'S';
+
+        System.out.println(testByte);
+        System.out.println(testShort);
+        System.out.println(testInt);
+        System.out.println(testLong);
+        System.out.println(testFloat);
+        System.out.println(testDouble);
+        System.out.println(testBoolean);
+        System.out.println(testString);
+        System.out.println(testChar);
+    }
+}
