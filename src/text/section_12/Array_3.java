c601ef54de3386b2664d26f15e9ebba40c032940
ich1zer0 <proton.ich1@protonmail.com>
2024-09-16 14:47:09 +0900

    Section11 コード追加

---
+++ src/text/section_12/Array_3.java
@@ -0,0 +1,11 @@
+package text.section_12;
+
+public class Array_3 {
+    public static void main(String[] args) {
+        int[] scoreArray = { 70, 85, 80, 95, 90 };
+
+        for(int score : scoreArray) {
+            System.out.println(score);
+        }
+    }
+}
