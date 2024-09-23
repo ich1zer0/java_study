97c2d2d580cce54243e055a9b35664ef5c5f34ad
ich1zer0 <proton.ich1@protonmail.com>
2024-09-23 17:38:11 +0900

    Section17 コード追加

---
+++ src/text/section_17/Keisyo_Human1.java
@@ -0,0 +1,11 @@
+package text.section_17;
+
+public class Keisyo_Human1 {
+    double weight;
+    double height;
+
+    double colcBmi(double weight, double height) {
+        return this.weight / (this.height * this.height);
+
+    }
+}
