58d15d4ea62c709f275256346d8ab40bfe3f6841
ich1zer0 <proton.ich1@protonmail.com>
2024-10-01 17:46:51 +0900

    Section20 コード追加

---
+++ src/text/section_20/Math_Test10.java
@@ -0,0 +1,11 @@
+package text.section_20;
+
+public class Math_Test10 {
+    public static void main(String[] args) {
+        double val1 = 125;
+        double val2 = -125;
+
+        System.out.println(Math.cbrt(val1));
+        System.out.println(Math.cbrt(val2));
+    }
+}
